d65f3123a12c9fdffe2e9cc6e104e93a328debad
Lucas Araujo <lucasaraujoo44.la@gmail.com>
2024-01-26 11:20:05 -0300

    Implementando o localStorage na aplicação.

---
+++ BlazorShop.Web/Services/GerenciaProdutosLocalStorageService.cs
@@ -0,0 +1,40 @@
+﻿using Blazored.LocalStorage;
+using BlazorShop.Models.DTOs;
+using System.Runtime.CompilerServices;
+
+namespace BlazorShop.Web.Services
+{
+	public class GerenciaProdutosLocalStorageService : IGerenciaProdutosLocalStorageService
+	{
+		private const string key = "ProdutoCollection";
+
+		private readonly ILocalStorageService localStorageService;
+		private readonly IProdutoService produtoService;
+		
+		public GerenciaProdutosLocalStorageService(ILocalStorageService localStorageService, IProdutoService produtoService)
+		{
+			this.localStorageService = localStorageService;
+			this.produtoService = produtoService;
+		}
+
+		public async Task<IEnumerable<ProdutoDto>> GetCollection()
+		{
+			return await this.localStorageService.GetItemAsync<IEnumerable<ProdutoDto>>(key) ?? await AddColletion();
+		}
+
+		public  async Task RemoveCollection()
+		{
+			await this.localStorageService.RemoveItemAsync(key);
+		}
+
+		private async Task<IEnumerable<ProdutoDto>> AddColletion() 
+		{
+			var produtoColletion = await this.produtoService.GetItens();
+			if(produtoColletion != null) 
+			{
+				await this.localStorageService.SetItemAsync(key, produtoColletion);
+			}
+			return produtoColletion;
+		}
+	}
+}
